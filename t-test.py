42500f5fd59db87adae9478eb29096ba4e787c48
Devi <45352250+DeviMaya23@users.noreply.github.com>
2025-09-10 15:36:40 +1000

    feat: update t test and add histogram

--- t-test.py
+++ t-test.py
@@ -5,7 +5,7 @@ def t_test():
     df = init.get_cleaned_dataset2()
     dfNoRat = df[df['rat_arrival_number'] == 0]
     dfNoRat = dfNoRat['bat_landing_number'].to_numpy()
-    dfWithRat = df[df['rat_arrival_number'] == 1]
+    dfWithRat = df[df['rat_arrival_number'] > 0]
     dfWithRat = dfWithRat['bat_landing_number'].to_numpy()
 
 
@@ -15,13 +15,13 @@ def t_test():
     print("Mean of dataset without rat arrivals", st.tmean(dfNoRat))
     print("P Value:", p_value)
 
-    # Mean of dataset with rats = mu1
-    # Mean of dataset without rats = mu2
+    # Mean of dataset with rats = x_bar1
+    # Mean of dataset without rats = x_bar2
     # Our null hypothesis is that rats are not affecting bats behaviour
     # when it comes to food. Therefore, the amount of bats landing
     # should be unaffected by rats.
-    # H0 -> mu1 = mu2
-    print("Null hypothesis: mu1 == mu2")
+    # H0 -> x_bar1 = x_bar2
+    print("Null hypothesis: x_bar1 == x_bar2")
     print("P value is < 0.05, therefore the null hypothesis is invalid.")
 
 t_test()
